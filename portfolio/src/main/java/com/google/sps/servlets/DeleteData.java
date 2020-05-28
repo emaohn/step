e02b246bf1092b7c9e0a55ae253ce1473a8905a3
Emmie Ohnuki <emaohn@gmail.com>
2020-05-28 18:14:14 -0400

    Deletes all comments from datastore

---
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteData.java
@@ -0,0 +1,34 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.gson.Gson;
+import com.google.sps.data.Comment;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/delete-data")
+public class DeleteData extends HttpServlet {
+  private DatastoreService datastore;
+
+  public DeleteData() {
+    this.datastore = DatastoreServiceFactory.DatastoreService();
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    Query query = new Query("Comment");
+    PreparedQuery results = datastore.prepare(query);
+    for (Entity entity: results.asIterable()) {
+        datastore.delete(entity.getKey());
+    }
+    response.sendRedirect("/index.html");
+  }
+}
