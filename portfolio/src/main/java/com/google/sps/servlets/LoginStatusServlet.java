e87867fafaf3b62009c85d2938c34b20a0c37e32
Emmie Ohnuki <emaohn@gmail.com>
2020-06-02 17:35:54 +0000

    returns logins status as json with boolean and login/logout url

--- portfolio/src/main/java/com/google/sps/servlets/LoginStatusServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginStatusServlet.java
@@ -14,14 +14,28 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import com.google.sps.data.LoginStatus;
 
 @WebServlet("/login-status")
 public class LoginStatusServlet extends HttpServlet {
+  UserService userService = UserServiceFactory.getUserService();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html");
-    UserService userService = UserServiceFactory.getUserService();
-    response.getWriter().println(userService.isUserLoggedIn());
+    String statusJson = convertToJSON(userService.isUserLoggedIn());
+    response.setContentType("application/json");
+    response.getWriter().println(statusJson);
+  }
+
+  private String convertToJSON(boolean status) {
+    Gson gson = new Gson();
+    LoginStatus loginStats;
+    if (status) {
+      loginStats = new LoginStatus(status, userService.createLogoutURL("/index.jsp"));
+    } else {
+      loginStats = new LoginStatus(status, userService.createLoginURL("/index.jsp"));
+    }
+    return gson.toJson(loginStats);
   }
 }
