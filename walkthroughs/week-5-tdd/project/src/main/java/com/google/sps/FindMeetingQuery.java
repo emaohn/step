b25945f5d7912a368c64e3b75ff0bb55f9ef4cff
Emmie Ohnuki <emaohn@gmail.com>
2020-06-04 16:53:07 +0000

    Separate timerange modification into separate function for reusibility

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -27,14 +27,22 @@ public final class FindMeetingQuery {
     timeranges.add(TimeRange.WHOLE_DAY);
     Collection<String> meetingAttendants = request.getAttendees();
     for(Event e: events) {
-      Set<String> eventAttendants = e.getAttendees();
-      for(String attendee: meetingAttendants) {
+      for(String attendee: e.getAttendees()) {
         // if one of the attendees of this event is one of the people we're scheduling this meeting for, then find any time conflicts and remove them
-        if (eventAttendants.contains(attendee)) {
-          for (int i = 0; i < timeranges.size();) {
+        if (meetingAttendants.contains(attendee)) {
+            modifyTimeRanges(timeranges, e, request);
+        }
+      }
+    }
+    // See if my optional attendees can make any of these time slots
+
+    return timeranges;
+  }
+
+    private void modifyTimeRanges(ArrayList<TimeRange> timeranges, Event e, MeetingRequest request) {
+        for (int i = 0; i < timeranges.size();) {
             TimeRange curRange = timeranges.get(i);
             TimeRange eventRange = e.getWhen();
-
             if (curRange.overlaps(eventRange)) {
                 // case 1: current timerange is within event timerange => no part of timerange works so remove
                 if (eventRange.contains(curRange)) {
@@ -43,10 +51,9 @@ public final class FindMeetingQuery {
                 } else if (curRange.contains(eventRange)) {
                     int numNewRanges = 0;
                     if (eventRange.start() - curRange.start() >= request.getDuration()) {
-                        timeranges.add(i + (++numNewRanges), TimeRange.fromStartEnd(curRange.start(), eventRange.start(), false));
-                    }
+                        timeranges.add(i + (++numNewRanges), TimeRange.fromStartEnd(curRange.start(), eventRange.start(), false));                    }
                     if (curRange.end() - eventRange.end() >= request.getDuration()) {
-                        timeranges.add(i + (++numNewRanges), TimeRange.fromStartEnd(eventRange.end(), curRange.end(), false));
+                    timeranges.add(i + (++numNewRanges), TimeRange.fromStartEnd(eventRange.end(), curRange.end(), false));
                     }
                     timeranges.remove(i);
                     i += numNewRanges;
@@ -62,16 +69,13 @@ public final class FindMeetingQuery {
                         timeranges.remove(i);
                     }
                 }
+            // if timerange is after event, don't need to look at this or the rest since they're in order
+            } else if (curRange.start() > eventRange.end()) {
+                break;
             } else {
                 //if no overlap then timerange is safe, keep untouched
                 i++;
             }
-          }
-          // no need to check other people in the same event
-          break;
         }
-      }
     }
-    return timeranges;
-  }
 }
